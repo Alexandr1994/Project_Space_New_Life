58483a28aa97ba319220cdb23a812a381e2a4733
Alexandr Fakhrutdinov <alexfahat@gmail.com>
2015-04-25 23:13:46 +0600

    Beginning
    >uploaded project
    >included and tested SFML

---
+++ Project Space - New Live/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+
+namespace Project_Space___New_Live
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.main();
+        }
+    }
+
+    class Test
+    {
+
+        static VideoMode testMode = new VideoMode(640, 480);
+        Window testWindow = new Window(testMode, "Test");
+
+
+        public void main()
+        {           
+            testWindow.Display();
+            Thread.Sleep(1000);
+        }
+
+    }
+}
+
